f4f31a120e65184853c9e64072c8984f7225fafa
Taras Chaikovskyi <chaikovskyit@gmail.com>
2023-09-18 21:22:15 +0300

    added function for join classes, and added git ignore

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,13 @@ import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import './styles/index.scss';
 
 import { useTheme } from './theme/useTheme';
+import { classNames } from './helpers/classNames/classNames';
 
 const App = () => {
 	const { theme, toggleTheme } = useTheme();
 
 	return (
-		<div className={`app ${theme}`}>
+		<div className={classNames('app', {}, [theme])}>
 			<button onClick={toggleTheme}>Theme</button>
 			<Link to={'/'}>Main</Link>
 			<Link to={'/about'}>About</Link>
